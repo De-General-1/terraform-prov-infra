5da7101596a68da76d34cf8374c3c80c28bdfacb
De-General-1 <aboagyefafa12@gmail.com>
2025-04-14 16:25:31 +0000

    terraform infra

--- ReadMe.md
+++ ReadMe.md
@@ -1,5 +1,4 @@
 
-
 ```markdown
 # 🛠️ Terraform AWS Infrastructure Project
 
--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
-# terraform {
-#   backend "s3" {
-#     bucket       = "modular-terraform-bucket-state-degen"
-#     key          = "terraform/terraform.tfstate"
-#     region       = "eu-west-1"
-#     encrypt      = true
-#     use_lockfile = true
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket       = "modular-terraform-bucket-state-degen"
+    key          = "terraform/terraform.tfstate"
+    region       = "eu-west-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
